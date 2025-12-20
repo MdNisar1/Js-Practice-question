58ab4c2bca2c8436c1ab79ac7ac481b1d4735df0
MdNisar1 <mdnisarktr62@gmail.com>
2025-12-20 19:59:12 +0530

    second question solved

--- script.js
+++ script.js
@@ -7,85 +7,9 @@
 // let age = 23;
 // const country = "india";
 
-// console.log(name,age,country);
-
-// console.log("hello developer!");
-// alert("welcome");
-// let user = prompt("emter your name:");
-// console.log("hi " + user);
-// console.log(user)
-
-// let message = "hello javascript";
-// console.log(message);
-
-// let str = "javaScript";
-// console.log(str.slice(0,4));
-
-// let na = "nisar";
-// console.log(`hello,${na}! kuch karoo`);
-
-// let fruits = "apple,banana,grapes";
-// console.log(fruits.split(","));
-
-// let text = "i love html";
-// console.log(text.replace("html","javascript"));
-
-// let sentence = "javascript is fun!";
-// console.log(sentence.includes("fun"));
-
-
-//............lecture27...java script basics...........//
-
-// console.log("hello world!");
-// console.log(10+5);
-
-// console.error("somthing went wrong!");
-// console.warn("this is warning!");
-// console.info("server started successfully!");
-
-// let user = {
-//     name: "nisar",
-//     email: "nisar@gamil.com",
-//     course: "javascript"
-// };
-// console.table(user);
-
-// console.group("user info");
-// console.log("name:nisar");
-// console.log("course:js");
-// console.groupEnd();
-
-// console.time("loopTime");
-// for(let i = 0; i<100000;i++){}     // isme dout hai
-// console.timeEnd("loopTime");
-
-// console.clear();
-
-// let name = "nisar md";
-// console.log(name);
-
-// let str = "javascript";
-// console.log(str.slice(0,4));
-
-// let name = "nisar";
-// console.log(`hello,${name}!welcome to js.`);
-
-// let freuits = "apple,banana,graps";
-// console.log(freuits.split(","))
-
-// let sentence = "i love html";
-// console.log(sentence.replace("html","javascript"));
-
-// let text = "learning javascript is fun!";
-// console.log(text.includes("fun"));
-
-// let a = 10;
-// if(a>5){
-//     console.log("a is greater than 5")
-// }
-
-// 2+3;
-// "hello"+"world";
-
-//...........lecture 28.....java script basics...........//
-
+let user = {
+    name: "nisar",
+    email: "nisar@gamil.com",
+    course: "javascript"
+};
+console.table(user);
